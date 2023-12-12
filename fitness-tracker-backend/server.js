c548492507dbc346771ef469563f4384f7f33f6d
mansur0108 <mansur.omary18@gmail.com>
2023-12-12 16:24:58 -0500

    Updated server.js to connect to MongoDB

--- fitness-tracker-backend/server.js
+++ fitness-tracker-backend/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
 
+// MongoDB connection
+const mongoDB = "mongodb://localhost:27017/fitnessTrackerDb";
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function () {
+  console.log("Connected successfully to MongoDB");
+});
+
+// Define a simple route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
